8035509b5125b056fb04b5a7ad9e4738e9bee494
Justin <justinmbell1997@gmail.com>
2025-07-22 20:10:28 -0500

    added rigidbody field

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
