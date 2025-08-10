5c7ee1b74f62b9132e037f5fc7e35eea7a7e02df
Justin <justinmbell1997@gmail.com>
2025-08-10 12:38:48 -0500

    added changes

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float deadZone = -45;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,5 +22,11 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + Vector3.left * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe deleted");
+            Destroy(gameObject);
+        }
     }
 }
